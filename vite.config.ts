d9a889e7611c43fe90874a48e3de072cd814f4d5
Liyw-liyw <liyuanwu_liyw@163.com>
2023-03-07 09:27:41 +0800

    feat: :art: add lint + commit check

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, normalizePath } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import viteEslint from 'vite-plugin-eslint';
+import viteStylelint from 'vite-plugin-stylelint';
+
+// 全局 scss 文件的路径
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve('./src/styles/variable.less'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteEslint({
+      exclude: ['**/*.spec.ts']
+    }),
+    viteStylelint()
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // additionalData 的内容会在每个 scss 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`
+      }
+    }
+  }
+});
